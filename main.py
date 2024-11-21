7b8beb1b7c3e2ecefbbe665dfee7e55c9873a36a
Daryna Pavlishchuk <dpavlishchuk@kse.org.ua>
2024-11-21 12:31:50 +0200

    uptaded func and took put of function

--- main.py
+++ main.py
@@ -17,26 +17,18 @@ def output_into_scr(file_address, medals, country, year_of_olympics, output, tex
             print(output_file.read())
 
 
-def user_input():
-    while True:
-        user_information = input("Enter your data:\n")
-        data = user_information.split(" ")
-        if len(data) == 5:
-            file_address = data[1]
-            medals = data[2]
-            country = data[3]
-            year_of_olympic = data[4]
-            return file_address, medals, country, year_of_olympic
-        elif len(data) == 7:
-            file_address = data[1]
-            medals = data[2]
-            country = data[3]
-            year_of_olympic = data[4]
+while True:
+    user_information = input("Enter your data:\n")
+    data = user_information.split(" ")
+    if len(data) >= 5:
+        file_address = data[1]
+        medals = data[2]
+        country = data[3]
+        year_of_olympic = data[4]
+        output_into_scr(file_address, medals, country, year_of_olympic)
+        if len(data) == 7:
             output = data[5]
             result_output = data[6]
-            return file_address, medals, country, year_of_olympic, output, result_output
-        else:
-            print("Invalid input, please try again")
-
-
-user_input()
+            output_into_scr(file_address, medals, country, year_of_olympic, output, result_output)
+    else:
+        print("Invalid input, please try again")
