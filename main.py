e5e568ebc56dd8ddbaa31d9ca3d9c1c4fff50585
Daryna Pavlishchuk <dpavlishchuk@kse.org.ua>
2024-11-27 18:08:54 +0200

    argparse func

--- main.py
+++ main.py
@@ -1,34 +1,46 @@
-def output_into_scr(file_address, medals, country, year_of_olympics, output, text_file):
-    create_file = open(text_file, 'w')
+import argparse
+
+parser = argparse.ArgumentParser("Information from user about olympic data")
+
+parser.add_argument("address_file", help="File with data")
+parser.add_argument("medals")
+parser.add_argument("total", help="optional")
+parser.add_argument("overall",  help="optional")
+parser.add_argument("country", nargs="*")
+parser.add_argument("year")
+parser.add_argument("output",  help="optional")
+parser.add_argument("result_file",  help="optional")
+
+args = parser.parse_args()
+
+
+def total_medals(file_address, total, year_input, output="", result_file=""):
+    result ={}
     with open(file_address, 'r') as olympics_data:
         for line in olympics_data:
-            if (country in line) and (year_of_olympics in line):
-                list_of_information = line.split("\t")
-                name = list_of_information[1]
-                discipline = list_of_information[12]
-                medal_type = list_of_information[14]
-                if output != "-output":
-                    print(f"{name} - {discipline} - {medal_type}")
-                else:
-                    with open("create_file", 'a') as output_file:
-                        output_file.write(f"{name} - {discipline} - {medal_type}")
-    if output == "-output":
-        with open("create_file", 'r') as output_file:
-            print(output_file.read())
+            if "total" in total:
+                data_list = line.split('\t')
+                country = data_list[6]
+                year_of_olympic = data_list[10]
+                medal_type = data_list[14].strip()
+                if year_input == year_of_olympic and year_of_olympic in line:
+                    if medal_type != "NA":
+                        if country not in result:
+                            result[country] = medal_type
+                        elif country in result:
+                            result[country] += medal_type
+    if output != "output":
+        for country, medal_type in result.items():
+            print(f"{country} - {medal_type}\n")
+    if output == "output":
+        with open(result_file, "w") as file:
+            for i, result in enumerate(result):
+                file.write(result + "\n")
+            for country, medal in result.items():
+                result_file.write(f"{country} - {medal_type}\n")
 
 
-while True:
-    user_information = input("Enter your data:\n")
-    data = user_information.split(" ")
-    if len(data) >= 5:
-        file_address = data[1]
-        medals = data[2]
-        country = data[3]
-        year_of_olympic = data[4]
-        output_into_scr(file_address, medals, country, year_of_olympic)
-        if len(data) == 7:
-            output = data[5]
-            result_output = data[6]
-            output_into_scr(file_address, medals, country, year_of_olympic, output, result_output)
-    else:
-        print("Invalid input, please try again")
+
+# total_medals(args.address_file, args.total, args.year)
+
+
