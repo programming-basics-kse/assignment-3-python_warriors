274869a0736f988af353fabe12c804606d23da8a
Daryna Pavlishchuk <dpavlishchuk@kse.org.ua>
2024-11-28 09:15:29 +0200

    2 task in main.py

--- main.py
+++ main.py
@@ -14,30 +14,33 @@ parser.add_argument("result_file",  help="optional")
 args = parser.parse_args()
 
 
-def total_medals(file_address, total, year_input, output="", result_file=""):
+def total_medals(file_address, year_input, output="", result_file=""):
     result ={}
+    gold = 0
+    silver = 0
+    bronze = 0
     with open(file_address, 'r') as olympics_data:
         for line in olympics_data:
-            if "total" in total:
-                data_list = line.split('\t')
-                country = data_list[6]
-                year_of_olympic = data_list[10]
-                medal_type = data_list[14].strip()
-                if year_input == year_of_olympic and year_of_olympic in line:
-                    if medal_type != "NA":
-                        if country not in result:
-                            result[country] = medal_type
-                        elif country in result:
-                            result[country] += medal_type
-    if output != "output":
-        for country, medal_type in result.items():
-            print(f"{country} - {medal_type}\n")
-    if output == "output":
-        with open(result_file, "w") as file:
-            for i, result in enumerate(result):
-                file.write(result + "\n")
-            for country, medal in result.items():
-                result_file.write(f"{country} - {medal_type}\n")
+            data_list = line.split('\t')
+            country = data_list[6]
+            year_of_olympic = data_list[10]
+            medal_type = data_list[14].strip()
+            if year_input == year_of_olympic in line:
+                for country in line:
+                    if medal_type == "Gold":
+                        gold  += 1
+                        result[country] = gold
+                    elif medal_type == "Silver":
+                        silver += 1
+                        result[country] = silver
+                    elif medal_type == "Bronze":
+                        bronze += 1
+                    result[country] = {[gold], [silver], [bronze]}
+        for country, medals in result.items():
+            print(f"{country} - {medals["gold"]} - {medals["silver"]} - {medals["bronze"]}")
+
+
+total_medals("OlympicAthletes-athlete_events.tsv", "2000")
 
 
 
